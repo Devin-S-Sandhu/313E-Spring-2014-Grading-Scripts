58f3179e72a85c06bd35e53935b9fd07e73707be
Mirrorkirby <devin.sandhu@gmail.com>
2014-01-29 19:46:19 -0600

    converts tab seperated comment files into individual files

---
+++ explode_output_file.py
@@ -0,0 +1,31 @@
+#run in the same directory as the file you're exploding (Where the grading script was run from)
+import sys
+
+if (len(sys.argv) == 2):
+  f = open(sys.argv[1], 'r')
+else:
+  print("Please have the name of the file to explode as a command line arguement")
+  exit()
+
+
+skip_first = True
+for line in f:
+  #skips the "CSID Grade Comments" line
+  if(skip_first):
+    skip_first = False
+    continue
+
+  vals = line.strip().split('\t')
+  #if they're no comments there's no file to write
+  if(len(vals) == 3):
+    fileName = vals[0] + '/' + sys.argv[1]
+    try:
+      f2 = open(fileName,'w')
+      f2.write('Grade: ' + vals[1] +'\n')
+      f2.write('Comments: ' + vals[2])
+      f2.close()
+      print('Wrote ' + fileName)
+    except:
+      print('\tFailed to write '+ fileName)
+
+f.close()
