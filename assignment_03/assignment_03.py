b87c5c8aca19e9c39cb2fc28dcd67c55a377c02c
dmonroy93 <dmonroy@utexas.edu>
2014-02-10 12:55:31 -0600

    Test cases work but script is unfinished

---
+++ assignment_03/assignment_03.py
@@ -0,0 +1,192 @@
+from os.path import os, exists
+from datetime import datetime, timedelta
+from functools import *
+import math
+import subprocess
+import sys
+import re
+import difflib
+import time
+
+pipes = {'stdout':subprocess.PIPE, 'stdin':subprocess.PIPE, 'stderr':subprocess.PIPE}
+
+outputFilename = 'assignment_03.txt'
+outputFile = open(outputFilename, 'a')
+inputFile = open('geometry.txt', 'r')
+inputLines = inputFile.read().splitlines()
+correctFile = open('correct.txt', 'r')
+correctLines = correctFile.read().splitlines()
+filename = "Geometry.py"
+dateString = "1-31-2014 23:00:00"
+
+def main():
+  out = subprocess.getoutput('ls ./')
+  CSIDS = out.split("\n")
+  if len(sys.argv) == 3:
+    outputFile.write('CSID\tGrade\tComments\n')
+    lowerBound = sys.argv[1]
+    upperBound = sys.argv[2] + '~';
+    myList = []
+    count = 0
+    for item in CSIDS:
+      if lowerBound <= item <= upperBound:
+        if "." not in item :
+          myList.append(item)
+    for csid in myList :
+      count += 1
+      os.system('clear')
+      print('======================')
+      print(csid + " " + str(count) + " out of " + str(len(myList)))
+      print('======================')
+      assign01(csid, True)
+  #singleton mode
+  else:
+    csid = sys.argv[1]
+    os.system('clear')
+    print('======================')
+    print(csid)
+    print('======================')
+    assign01(csid, False)
+  outputFile.close()
+  inputFile.close()
+
+def assign01(csid, writeToFile) :
+  fileToGrade = ""
+  late = 0
+  grade = 70
+  style = 30
+  wrongFileName = False
+  header = True
+  comments = []
+
+  os.chdir(csid)
+  if writeToFile: outputFile.write(csid + "\t")
+  files = os.listdir('.')
+
+  #filename checking
+  for f in files :
+    splitted = subprocess.getoutput('ls -l ' + f).split()
+    if f == filename :
+      fileToGrade = filename
+      late = isLate(splitted)
+      break
+    elif f == filename.lower() :
+      fileToGrade = filename.lower()
+      late = isLate(splitted)
+      wrongFileName = True
+      break
+
+  #really odd filename
+  if fileToGrade == "" :
+    print(subprocess.getoutput('ls -l'))
+    fileToGrade = input("Which file should I grade? ")
+    if fileToGrade == "" :
+      if writeToFile:
+        outputFile.write("0\tno file\n")
+      os.chdir("..")
+      return
+    else :
+      splitted = subprocess.getoutput('ls -l ' + fileToGrade.replace(' ','\ ')).split()
+      late = isLate(splitted)
+      wrongFileName = True
+
+  #grading time!
+  '''
+  tests 1-5: 10 pts ea
+  tests 6-9: 5 pts ea
+  '''
+  if not (fileToGrade == '' and late != -1):
+    os.system('cp ../geometry.txt .')
+    outputLines = subprocess.getoutput('python3 ' + fileToGrade).splitlines()
+
+
+'''
+    minLen = min(len(outputLines), len(correctLines))
+    grade = 0
+    index = 0
+    testsFailed = []
+    for i in range(minLen):
+      if i < 5 and outputLines[i] == correctLines[i]:
+        grade += 10
+      elif i < 9 and outputLines[i] == correctLines[i]:
+        grade += 5
+      elif i == 4 and outputLines[i] == 'Remove 1 counters from Heap 1':
+        grade += 10
+      else:
+        print('Failed Test',i)
+        print('\tTheir Output')
+        print('\t' + outputLines[i])
+        print('\tCorrect Output')
+        print('\t' + correctLines[i])
+        testsFailed.append(i)
+
+    if len(testsFailed) != 0:
+      comments.append("Failed tests " + ' '.join(str(x) for x in testsFailed))
+    else:
+      print('Passed all tests =D')
+
+'''
+
+
+  #checking for header and style
+  input("Hit Enter to cat")
+  print(subprocess.getoutput('cat ' + fileToGrade))
+  headerInput = input("Header(y/n, hit enter for y): ")
+  if headerInput == 'y' or headerInput == '':
+    header = True
+  else :
+    header = False
+  style = input("Style/Other (Out of 30, hit enter for 30): ")
+  gen_comments = input("General Comments?: ").rstrip().lstrip()
+  gen_comments = gen_comments if len(gen_comments) is not 0 else "style"
+  if not style.isdigit():
+    style = 30
+  else :
+    style = int(style)
+  if (gen_comments != "style" or style != 30):
+    gen_comments += " (%+d)" % (style - 30)
+    comments.append("%s" % gen_comments)
+
+  #writing grade time!
+  if late == -1:
+    if writeToFile: outputFile.write('0\t More than 2 days late')
+    print('Late more than 2 days!')
+  else :
+    if late == 2:
+      comments.append("2 days late (-20)")
+      grade -= 20
+    elif late == 1:
+      comments.append("1 day late (-10)")
+      grade -= 10
+
+    if wrongFileName or not header:
+      grade -= 5
+      if wrongFileName and header:
+        comments.append("wrong filename (-5)")
+      elif header and not wrongFileName:
+        comments.append("malformed header (-5)")
+      else:
+        comments.append("wrong filename and malformed header (-5)")
+
+    if writeToFile: outputFile.write(str(grade+style) + "\t" + ', '.join(comments))
+
+  if writeToFile: outputFile.write('\n')
+  os.chdir("..")
+
+#returns the number of days late an assignment is
+def isLate(splitted):
+  dueDate = datetime.strptime(dateString,"%m-%d-%Y %H:%M:%S")
+  lateOne = dueDate + timedelta(days=1)
+  lateTwo = lateOne + timedelta(days=1)
+  lateSev = dueDate + timedelta(days=7)
+  turninDate = datetime.strptime(splitted[5] + " " +(("0" + splitted[6]) if len(splitted[6]) == 1 else splitted[6])+ " " + splitted[7] +" 2014", "%b %d %H:%M %Y")
+  if turninDate <= dueDate:
+    return 0
+  elif turninDate <= lateOne:
+    return 1
+  elif turninDate <= lateTwo:
+    return 2
+  else :
+    return -1
+
+main()
