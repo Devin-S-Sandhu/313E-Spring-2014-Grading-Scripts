79bd45b0eedd05debcec0ffce4b078b24327ccec
Bon Koo <bjkoo91@gmail.com>
2014-02-19 14:57:15 -0600

    added pair as possible type

--- assignment_04/poker_func.py
+++ assignment_04/poker_func.py
@@ -1,5 +1,5 @@
 mapping = { 'royal flush': 10, 'straight flush': 9, 'four of a kind': 8, 'full house': 7, 'flush': 6,
-            'straight': 5, 'three of a kind': 4, 'two pair': 3, 'one pair': 2, 'high card': 1,
+            'straight': 5, 'three of a kind': 4, 'two pair': 3, 'one pair': 2, 'pair': 2, 'high card': 1,
             'A': 14, 'K': 13, 'Q': 12, 'J': 11 }
 
 class card:
