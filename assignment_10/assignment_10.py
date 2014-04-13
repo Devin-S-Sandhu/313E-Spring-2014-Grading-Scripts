4f6fd1e7ebf77b4841bce4e60be325edd7441f96
Bon Koo <bjkoo91@gmail.com>
2014-04-12 21:23:50 -0500

    adding assignment 10 scripts

---
+++ assignment_10/assignment_10.py
@@ -0,0 +1,194 @@
+from os.path import os, exists
+from datetime import datetime, timedelta
+from functools import *
+import math
+import subprocess
+import sys
+import re
+import difflib
+import time
+
+pipes = {'stdout':subprocess.PIPE, 'stdin':subprocess.PIPE, 'stderr':subprocess.PIPE}
+
+outputFilename = 'assignment_10.txt'
+outputFile = open(outputFilename, 'a')
+filename = "TestLinkedList.py"
+dateString = "3-25-2014 23:30:00"
+
+def main():
+  out = subprocess.getoutput('ls ./')
+  CSIDS = out.split("\n")
+  if len(sys.argv) == 3:
+    outputFile.write('CSID\tGrade\tComments\n')
+    lowerBound = sys.argv[1]
+    upperBound = sys.argv[2] + '~';
+    myList = []
+    count = 0
+    for item in CSIDS:
+      if lowerBound <= item <= upperBound:
+        if "." not in item :
+          myList.append(item)
+    for csid in myList :
+      count += 1
+      os.system('clear')
+      print('======================')
+      print(csid + " " + str(count) + " out of " + str(len(myList)))
+      print('======================')
+      assign10(csid, True)
+
+  #singleton mode
+  else:
+    csid = sys.argv[1]
+    os.system('clear')
+    print('======================')
+    print(csid)
+    print('======================')
+    assign10(csid, False)
+  outputFile.close()
+
+def assign10(csid, writeToFile) :
+  fileToGrade = ""
+  late = 0
+  grade = 70
+  style = 30
+  wrongFileName = False
+  header = True
+  comments = []
+
+  os.chdir(csid)
+  if writeToFile: outputFile.write(csid + "\t")
+  files = os.listdir('.')
+
+  #filename checking
+  for f in files :
+    splitted = subprocess.getoutput('ls -l ' + f).split()
+    if f == filename :
+      fileToGrade = filename
+      late = isLate(splitted)
+      break
+    elif f == filename.lower() :
+      fileToGrade = filename.lower()
+      late = isLate(splitted)
+      wrongFileName = True
+      break
+
+  #really odd filename
+  if fileToGrade == "" :
+    print(subprocess.getoutput('ls -l'))
+    fileToGrade = input("Which file should I grade? ")
+    if fileToGrade == "" :
+      if writeToFile:
+        outputFile.write("0\tno file\n")
+      os.chdir("..")
+      return
+    else :
+      splitted = subprocess.getoutput('ls -l ' + fileToGrade.replace(' ','\ ')).split()
+      late = isLate(splitted)
+      wrongFileName = True
+
+  #grading time!
+  test = open('../runtests.py', 'r')
+  testFile = open('runtests.py', 'w')
+  testFile.write('from ' + fileToGrade.split('.')[0] + ' import *\n\n')
+  testFile.write(test.read())
+  test.close()
+  testFile.close()
+
+  cur, timeout = 0, 15
+  sub = subprocess.Popen(['python3', 'runtests.py'], **pipes)
+  while cur < timeout and sub.poll() is None:
+    cur += 1
+    time.sleep(1)
+
+  if sub.poll() is None:
+    sub.terminate()
+    grade = 0
+    comments.append('program did not terminate (-70)')
+    comments.append('contact your grader')
+  else:
+    output = sub.communicate()[0].decode('utf-8').split('=TestLinkedList=')[1].split('\n')
+    output = [line for line in output if line != '']
+
+    if output[0].isdigit():
+      grade = int(output[0])
+      for l in output[1:]:
+        comments.append(l.strip())
+    else:
+      grade = 0
+      comments.append('program crashed (-70)')
+      comments.append('contact your grader')
+
+  if grade == 70:
+    print("<('.')^ Perfection ^('.')>")
+  else:
+    print("Grade: %d/70"%grade)
+
+  #checking for header and style
+  input("Hit Enter to cat first 20 lines (header)")
+  print(subprocess.getoutput('head -20 ' + fileToGrade))
+  headerInput = input("Header(y/n, hit enter for y): ")
+  if headerInput == 'y' or headerInput == '':
+    header = True
+  else :
+    header = False
+  input("Hit Enter to cat whole file (style/comments)")
+  print(subprocess.getoutput('cat ' + fileToGrade))
+  style = input("Style/Other (Out of 30, hit enter for 30): ")
+  gen_comments = input("General Comments?: ").rstrip().lstrip()
+  gen_comments = gen_comments if len(gen_comments) is not 0 else "style"
+  if not style.isdigit():
+    style = 30
+  else :
+    style = int(style)
+  if (gen_comments != "style" or style != 30):
+    gen_comments += " (%+d)" % (style - 30)
+    comments.append("%s" % gen_comments)
+
+  #writing grade time!
+  if late == -1:
+    if writeToFile: outputFile.write('0\t More than 7 days late')
+    print('Late more than 7 days!')
+  else :
+    if late == 3:
+      comments.append("3-7 days late (-30)")
+      grade -= 30
+    elif late == 2:
+      comments.append("2 days late (-20)")
+      grade -= 20
+    elif late == 1:
+      comments.append("1 day late (-10)")
+      grade -= 10
+
+    if wrongFileName or not header:
+      grade -= 5
+      if wrongFileName and header:
+        comments.append("wrong filename (-5)")
+      elif header and not wrongFileName:
+        comments.append("malformed header (-5)")
+      else:
+        comments.append("wrong filename and malformed header (-5)")
+
+    if writeToFile: outputFile.write(str(grade+style) + "\t" + ', '.join(comments))
+
+  if writeToFile: outputFile.write('\n')
+  os.chdir("..")
+
+#returns the number of days late an assignment is
+def isLate(splitted):
+  dueDate = datetime.strptime(dateString,"%m-%d-%Y %H:%M:%S")
+  lateOne = dueDate + timedelta(days=1)
+  lateTwo = lateOne + timedelta(days=1)
+  lateSev = dueDate + timedelta(days=7)
+  turninDate = datetime.strptime(splitted[5] + " " +(("0" + splitted[6]) if len(splitted[6]) == 1 else splitted[6])+ " " + splitted[7] +" 2014", "%b %d %H:%M %Y")
+  if turninDate <= dueDate:
+    return 0
+  elif turninDate <= lateOne:
+    return 1
+  elif turninDate <= lateTwo:
+    return 2
+  elif turninDate <= lateSev:
+    return 3
+  else :
+    return -1
+
+main()
---
+++ assignment_10/runtests.py
@@ -0,0 +1,317 @@
+def insertFirst(ll, data):
+    newLink = Link(data)
+    newLink.next = getFirst(ll)
+    putFirst(ll, newLink)
+
+def makeLinkedList(items, addWorks):
+    ll = LinkedList()
+    for i in list(reversed(items)):
+        if addWorks:
+            ll.addFirst(i)
+        else:
+            insertFirst(ll, i)
+    return ll
+
+def getFirst(ll):
+    try:
+        return ll.first
+    except:
+        try:
+            return ll.head
+        except:
+            return None
+
+def putFirst(ll, link):
+    try:
+        ll.first = link
+    except:
+        try:
+            ll.head = link
+        except:
+            pass
+
+def diff(ll, ls):
+    cur = getFirst(ll)
+    for i in ls:
+        if cur.data != i:
+            return False
+        cur = cur.next
+    return cur != None
+
+'''
+grade out of 70 pts
+
+test 1 (sample test):
+{addInOrder, getNumLinks, find*, delete} = 1 pt (4 pts)
+{copyList, reverseList, sortList, isSorted, isEmpty, mergeList, isEqual, removeDuplicates} = 2 pts (16 pts)
+
+test 2:
+{find*, delete} = 1 pt
+{addInOrder, getNumLinks} = 4 pts
+{copyList, reverseList, sortList, isSorted, isEmpty, mergeList, isEqual, removeDuplicates} = 5 pts (40 pts)
+'''
+def runTests(run, list1, list2, addWorks):
+    if run == 1:
+        grade = 20
+    else:
+        grade = 50
+
+    comments = []
+
+    # addInOrder
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        list1.append(13)
+        list1.sort()
+        ll.addInOrder(13)
+        if diff(ll, list1):
+            raise
+    except:
+        cmt = 'failed addInOrder'
+        if run == 1:
+            grade -= 1
+            cmt += ' in test1 (-1)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-4)'
+        comments.append(cmt)
+
+    # getNumLinks
+    try:
+        ll1 = makeLinkedList(list1, addWorks)
+        ll2 = makeLinkedList(list2, addWorks)
+        if ll1.getNumLinks() != len(list1) or ll2.getNumLinks() != len(list2):
+            raise
+    except:
+        cmt = 'failed getNumLinks'
+        if run == 1:
+            grade -= 1
+            cmt += ' in test1 (-1)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-4)'
+        comments.append(cmt)
+
+    # find
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        if ll.findUnordered(13) == None:
+            raise
+        if ll.findUnordered(999) != None:
+            raise
+        if ll.findOrdered(13) == None:
+            raise
+        if ll.findOrdered(999) != None:
+            raise
+    except:
+        cmt = 'failed find'
+        if run == 1:
+            grade -= 1
+            cmt += ' in test1 (-1)'
+        else:
+            grade -= 1
+            cmt += ' in test2 (-1)'
+        comments.append(cmt)
+
+    # delete
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        list1.remove(13)
+        if ll.delete(13) == None or diff(ll, list1):
+            raise
+        if ll.delete(999) != None or diff(ll, list1):
+            raise
+    except:
+        cmt = 'failed delete'
+        if run == 1:
+            grade -= 1
+            cmt += ' in test1 (-1)'
+        else:
+            grade -= 1
+            cmt += ' in test2 (-1)'
+        comments.append(cmt)
+
+    # copyList
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        ll2 = ll.copyList()
+        if ll == ll2 or diff(ll2, list1):
+            raise
+    except:
+        cmt = 'failed copyList'
+        if run == 1:
+            grade -= 2
+            cmt += ' in test1 (-2)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-5)'
+        comments.append(cmt)
+
+    # reverseList
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        ll2 = ll.reverseList()
+        if ll == ll2 or diff(ll2, list(reversed(list1))):
+            raise
+    except:
+        cmt = 'failed reverseList'
+        if run == 1:
+            grade -= 2
+            cmt += ' in test1 (-2)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-5)'
+        comments.append(cmt)
+
+    # sortList
+    try:
+        ll = makeLinkedList(list2, addWorks)
+        ll2 = ll.sortList()
+        if ll == ll2 or diff(ll2, list(sorted(list2))):
+            raise
+    except:
+        cmt = 'failed sortList'
+        if run == 1:
+            grade -= 2
+            cmt += ' in test1 (-2)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-5)'
+        comments.append(cmt)
+
+    # isSorted
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        if not ll.isSorted():
+            raise
+        ll2 = makeLinkedList(list2, addWorks)
+        if ll2.isSorted():
+            raise
+    except:
+        cmt = 'failed isSorted'
+        if run == 1:
+            grade -= 2
+            cmt += ' in test1 (-2)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-5)'
+        comments.append(cmt)
+
+    # isEmpty
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        if ll.isEmpty():
+            raise
+        ll2 = makeLinkedList(list2, addWorks)
+        if ll2.isEmpty():
+            raise
+        ll3 = LinkedList()
+        if not ll3.isEmpty():
+            raise
+    except:
+        cmt = 'failed isEmpty'
+        if run == 1:
+            grade -= 2
+            cmt += ' in test1 (-2)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-5)'
+        comments.append(cmt)
+
+    # mergeList
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        ll2 = makeLinkedList(list2, addWorks)
+        ll3 = ll.mergeList(ll2)
+        merged = []
+        for i in list1:
+            merged.append(i)
+        for i in list2:
+            merged.append(i)
+        merged.sort()
+        if ll == ll3 or ll2 == ll3 or diff(ll3, merged):
+            raise
+    except:
+        cmt = 'failed mergeList'
+        if run == 1:
+            grade -= 1
+            cmt += ' in test1 (-2)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-5)'
+        comments.append(cmt)
+
+    # isEqual
+    try:
+        ll = makeLinkedList(list1, addWorks)
+        ll2 = makeLinkedList(list2, addWorks)
+        ll3 = makeLinkedList(list1, addWorks)
+        if ll.isEqual(ll2):
+            raise
+        if not ll.isEqual(ll3):
+            raise
+    except:
+        cmt = 'failed isEqual'
+        if run == 1:
+            grade -= 1
+            cmt += ' in test1 (-2)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-5)'
+        comments.append(cmt)
+
+    # removeDuplicates
+    try:
+        list1.extend([13, 14, 13, 13, 13, 14, 17, 17, 17, 18, 2, 2, 5, 5])
+        list1.sort()
+        ll = makeLinkedList(list1, addWorks)
+        noDupl = list(set(list1))
+        noDupl.sort()
+        ll2 = ll.removeDuplicates()
+        if ll == ll2 or diff(ll2, noDupl):
+            raise
+    except:
+        cmt = 'failed removeDuplicates'
+        if run == 1:
+            grade -= 1
+            cmt += ' in test1 (-2)'
+        else:
+            grade -= 4
+            cmt += ' in test2 (-5)'
+        comments.append(cmt)
+
+    return grade, comments
+
+print ('\n=TestLinkedList=')
+
+addWorks = True
+
+try:
+    test = [1, 3, 5, 7, 9]
+    testll = LinkedList()
+
+    for t in test:
+        testll.addFirst(t)
+
+    cur = getFirst(testll)
+    for t in test:
+        if cur.data != t:
+            addWorks = False
+            break
+        cur = cur.next
+except:
+    addWorks = False
+
+l1 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+l2 = [19, 15, 13, 11, 17]
+grade, comments = runTests(1, l1, l2, addWorks)
+l1 = [12, 23, 7, 34, 80, 20, 40, 123, 53, 2, 12, 15, 16, 12, 19, 52, 61, 77, 52]
+l2 = [32, 62, 32, 52, 16, 73, 84, 35]
+gr, cmt = runTests(2, l1, l2, addWorks)
+grade += gr
+comments.extend(cmt)
+
+print (str(grade))
+for l in comments:
+    print (l)
+print('finished')
